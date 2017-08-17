dc4e806e15c6d49a4ac819835f7b6279faddb98c
Air-TR <tr1838@163.com>
2017-08-17 09:57:04 +0800

    1

---
+++ src/test/java/MybatisGenerator.java
@@ -0,0 +1,31 @@
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class MybatisGenerator {
+    public static void main(String[] args) {
+        for (Entry<Object, Object> en:System.getProperties().entrySet()) {
+            System.out.println(en.getKey()+"==>"+en.getValue());
+        }
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generator_config.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+    }
+}
