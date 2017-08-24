ab3b6b9b1b97eb2008f6bf6844a317c468c6845f
Air-TR <tr1838@163.com>
2017-08-24 17:14:11 +0800

    1

--- src/test/java/MybatisGenerator.java
+++ src/test/java/MybatisGenerator.java
@@ -10,22 +10,22 @@ import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class MybatisGenerator {
-    public static void main(String[] args) {
-        for (Entry<Object, Object> en:System.getProperties().entrySet()) {
-            System.out.println(en.getKey()+"==>"+en.getValue());
-        }
-        try {
-            List<String> warnings = new ArrayList<String>();
-            boolean overwrite = true;
-            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-            InputStream is = classloader.getResourceAsStream("generator_config.xml");
-            ConfigurationParser cp = new ConfigurationParser(warnings);
-            Configuration config = cp.parseConfiguration(is);
-            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-            myBatisGenerator.generate(null);
-        } catch (Exception e) {
-           e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		for (Entry<Object, Object> en : System.getProperties().entrySet()) {
+			System.out.println(en.getKey() + "==>" + en.getValue());
+		}
+		try {
+			List<String> warnings = new ArrayList<String>();
+			boolean overwrite = true;
+			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+			InputStream is = classloader.getResourceAsStream("generator_config.xml");
+			ConfigurationParser cp = new ConfigurationParser(warnings);
+			Configuration config = cp.parseConfiguration(is);
+			DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+			MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+			myBatisGenerator.generate(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
